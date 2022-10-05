e5f408963dc41585aa2e77387950a28f4aa6fd2d
Preetansha Agrawal <preetanshaagrawal@gmail.com>
2022-10-05 19:50:20 +0530

    first commit

---
+++ mainBeats.js
@@ -0,0 +1,30 @@
+function beatingDrums(e){
+
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+// function removeTransition(e){
+//     console.log("playing");
+//     if(e.propertyName !== 'transform') return;
+//     this.classList.remove('playing');
+// }
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', beatingDrums);
